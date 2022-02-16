bb4411c8d385c52d03e7621575f823b402355930
mishaniahomi <69090763+mishaniahomi@users.noreply.github.com>
2022-02-16 17:35:01 +0300

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from wtforms.validators import Length
+
+
+class ContactForm(FlaskForm):
+    name = StringField('Имя', validators=[DataRequired()])
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import json
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from MO import check
+#  форма для обратной сзвязи
+from forms import ContactForm
+
 app = Flask(__name__)
-
-
+app.config['SECRET_KEY'] = "12345"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -17,25 +20,48 @@ class urls(db.Model):
     def __repr__(self):
         return '<Article %r>' % self.id
 
-@app.route('/', methods= ['POST', 'GET'])
-def main():
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = ContactForm()
+
+    return render_template("index.html",
+                           title="index page",
+                           form=form)
+
+@app.route('/newdata', methods  = ['GET'])
+def new_data():
+        data = urls.query.order_by(urls.date).all()
+        return render_template("data.html", data=data)
+
+
+@app.route('/update', methods = ['GET'])
+def update():
+    if request.method == 'GET':
+        table = "<table border='1'>"
+        for i in urls.query.order_by(urls.date).all():
+            table+="<tr><td>{}</td><td>{}</td><td>{}</td></tr>".format(i.id, i.title, i.date)
+        table+="</table>"
+        return json.dumps({'success': 'true', 'msg': table})
+    else:
+        return json.dumps({'success': 'false', 'msg': 'Ошибка на сервере!'})
+
+@app.route('/send', methods=['POST'])
+def send():
+    form = ContactForm()
     if request.method == "POST":
-        new_data = request.form["text"]
-        data = urls(title=new_data)
-        try:
+        if form.validate_on_submit():
+            url = request.form.get('name')
+            data = urls(title=url)
             db.session.add(data)
             db.session.commit()
-            note = check(new_data)
-            flag = True
-            return render_template("index.html", url = new_data, note = note, flag = flag)
-        except:
-            return "Ошибка"
-    else:
-        return render_template("index.html")
+            answer = "{} is {}".format(url, check(url) )
+            return json.dumps({'success': 'true', 'msg': answer})
+        else:
+            return json.dumps({'success': 'false', 'msg': 'Ошибка на сервере!'})
 
-@app.route('/newdata')
-def new_data():
-    data = urls.query.order_by(urls.date).all()
-    return render_template("data.html",data = data)
-if __name__ == "__main__":
+
+
+
+
+if __name__ == '__main__':
     app.run(debug=True)
